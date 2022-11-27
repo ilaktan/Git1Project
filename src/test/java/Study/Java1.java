8bda331869ae9f083af0f95352f02285ffb3637b
ilaktan <ilaktan@gmail.com>
2022-11-27 10:29:47 +0000

    Ilhami Commit

---
+++ src/test/java/Study/Java1.java
@@ -0,0 +1,12 @@
+package Study;
+
+public class Java1 {
+    public static void main(String[] args) {
+        int fatihAge= 11;
+        int betulAge=13;
+        int totalAge=fatihAge+betulAge;
+
+        System.out.println("total Age"+ totalAge);
+
+    }
+}
